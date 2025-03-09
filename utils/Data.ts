b14c3a248dfcf92feff25039d9b679debf42d62b
Alex Bhusal <alexbhusal@icloud.com>
2025-03-10 01:21:02 +0545

    ui

--- utils/Data.ts
+++ utils/Data.ts
@@ -1,35 +1,61 @@
 export const FRONTEND_TECHS = [
   { name: "React", time: 10 },
-  { name: "Vue", time: 8 },
-  { name: "Angular", time: 12 },
+  { name: "Vue", time: 12 },
+  { name: "Angular", time: 13 },
   { name: "Svelte", time: 9 },
-  { name: "Ember", time: 15 },
-  { name: "Next.js", time: 11 },
+  { name: "Ember", time: 12 },
+  { name: "Next.js", time: 14 },
+  { name: "Remix.js", time: 18 },
 ];
 
 export const BACKEND_TECHS = [
   { name: "Node.js", time: 15 },
   { name: "Django", time: 14 },
+  { name: "Laravel", time: 15 },
+  { name: "ASP.NET", time: 17 },
   { name: "Ruby on Rails", time: 18 },
   { name: "Laravel", time: 16 },
   { name: "Spring Boot", time: 20 },
   { name: "Flask", time: 13 },
+  { name: "Go (Golang)", time: 18 },
+  { name: "NestJS", time: 15 },
 ];
 
 export const DATABASES = [
   { name: "MySQL", time: 10 },
   { name: "MongoDB", time: 12 },
   { name: "PostgreSQL", time: 11 },
+  { name: "OracleDB", time: 15 },
   { name: "SQLite", time: 8 },
   { name: "MariaDB", time: 9 },
   { name: "Redis", time: 7 },
+  { name: "FireBase", time: 7 },
 ];
 
 export const ALGORITHMS = [
-  { name: "Simple", time: 10 },
-  { name: "Medium", time: 20 },
-  { name: "Complex", time: 30 },
+  { name: "K-Nearest Neighbors (KNN)", time: 20 },
+  { name: "Convolutional Neural Network (CNN)", time: 50 },
+  { name: "Support Vector Machine (SVM)", time: 30 },
+  { name: "Decision Tree", time: 25 },
+  { name: "Random Forest", time: 35 },
+  { name: "Logistic Regression", time: 15 },
+  { name: "Linear Regression", time: 10 },
+  { name: "Naive Bayes", time: 18 },
+  { name: "K-Means Clustering", time: 25 },
+  { name: "Principal Component Analysis (PCA)", time: 22 },
+  { name: "Recurrent Neural Network (RNN)", time: 40 },
+  { name: "Long Short-Term Memory (LSTM)", time: 45 },
+  { name: "Gradient Boosting", time: 40 },
+  { name: "XGBoost", time: 30 },
+  { name: "AdaBoost", time: 30 },
+  { name: "Deep Neural Network (DNN)", time: 50 },
+  { name: "Generative Adversarial Network (GAN)", time: 55 },
+  { name: "Markov Chains", time: 28 },
+  { name: "Bayesian Networks", time: 35 },
+  { name: "Other", time: 32 }
+
 ];
+
 
 export const WEEK_OPTIONS = [
   { name: "1 week", time: 35 },
